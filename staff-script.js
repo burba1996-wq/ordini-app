c9d71a7b474f76491c1e4b1d7bfcd18ccb2d45d1
burba1996-wq <burba1996@gmail.com>
2025-12-10 18:45:25 +0100

    Update staff-script.js

--- staff-script.js
+++ staff-script.js
@@ -1,9 +1,12 @@
-// --- 1. CONFIGURAZIONE FIREBASE (USA LA TUA) ---
+// --- 1. CONFIGURAZIONE FIREBASE ---
 const firebaseConfig = {
-    apiKey: "AIzaSyC0SFan3-K074DG5moeqmu4mUgXtxCmTbg",
-    authDomain: "menu-6630f.firebaseapp.com",
-    projectId: "menu-6630f",
-    // ... altri campi
+  apiKey: "AIzaSyC0SFan3-K074DG5moeqmu4mUgXtxCmTbg",
+  authDomain: "menu-6630f.firebaseapp.com",
+  projectId: "menu-6630f",
+  storageBucket: "menu-6630f.firebasestorage.app",
+  messagingSenderId: "250958312970",
+  appId: "1:250958312970:web:9a7929c07e8c4fa352d1f3",
+  measurementId: "G-GTQS2S4GNF"
 };
 
 // Inizializzazione Firebase
