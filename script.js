6bab41cb12a34ef2b075554508ed8cd85c85e91c
burba1996-wq <burba1996@gmail.com>
2025-12-10 15:48:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0SFan3-K074DG5moeqmu4mUgXtxCmTbg",
+  authDomain: "menu-6630f.firebaseapp.com",
+  projectId: "menu-6630f",
+  storageBucket: "menu-6630f.firebasestorage.app",
+  messagingSenderId: "250958312970",
+  appId: "1:250958312970:web:9a7929c07e8c4fa352d1f3",
+  measurementId: "G-GTQS2S4GNF"
+};
