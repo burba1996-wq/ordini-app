bf6ebf6e974a709a9b90a98a0e9ecc105fff494a
burba1996-wq <burba1996@gmail.com>
2025-12-10 15:18:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+type="module"
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import {
   getFirestore,
@@ -116,4 +116,5 @@ function initStaff(){
     });
   });
 }
+
 
